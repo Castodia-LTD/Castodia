9a15665ccb4c28c8dda005722cbdf8e4e2abe4a8
Castodia-LTD <admin@castodia.co.uk>
2026-09-25 07:08:13 +0100

    Attach feature keys to support navigation

--- config/navigation/careSupportNavigation.ts
+++ config/navigation/careSupportNavigation.ts
@@ -11,25 +11,30 @@ export const careSupportNavigation: AppShellLink[] = [
     href: "/care/support/rota",
     label: "My rota",
     icon: "calendar",
+    featureKey: "rota",
   },
   {
     href: "/care/support/timelines",
     label: "Timelines",
     icon: "timelines",
+    featureKey: "timelines",
   },
   {
     href: "/care/support/service-users",
     label: "People",
     icon: "service-users",
+    featureKey: "people",
   },
   {
     href: "/care/support/handovers",
     label: "Handovers",
     icon: "handovers",
+    featureKey: "handovers",
   },
   {
     href: "/care/support/reporting/safeguarding",
     label: "Safeguarding",
     icon: "safeguarding",
+    featureKey: "safeguarding",
   },
 ];
